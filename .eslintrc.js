9c6579b59040147dc6dec4837cf75eb544089756
shihyao001 <shihyao001@ibears.io>
2024-07-07 20:12:01 +0800

    try to fix deploy issue

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,9 +7,7 @@ module.exports = {
 		es2021: true,
 	  },
 	  extends: [
-		'eslint:recommended',
-		'plugin:react/recommended',
-		'plugin:import/recommended',
+		'react-app', 'react-app/jest'
 	  ],
 	  globals: {
 		jest: 'readonly',
